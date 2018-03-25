a6ef4738385c4db9dc7228f50a21d18ed26ff802
Abhiraj Suryawanshi <abhiraj.suryawanshi@smu.ca>
2018-03-25 00:32:13 -0300

    Added Customer Model

---
+++ MovieBookingRest/src/org/smu/model/Customer.java
@@ -0,0 +1,78 @@
+package org.smu.model;
+
+public class Customer {
+
+	private int Id;
+	private String firstName;
+	private String lastName;
+	private String city;
+	private String province;
+	private String country;
+	private String postal;
+	private String phone;
+	private String email;
+	private String address;
+	
+	public int getId() {
+		return Id;
+	}
+	public void setId(int id) {
+		Id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getProvince() {
+		return province;
+	}
+	public void setProvince(String province) {
+		this.province = province;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getPostal() {
+		return postal;
+	}
+	public void setPostal(String postal) {
+		this.postal = postal;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+}
